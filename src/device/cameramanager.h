fdee2a2d695143725a590a71dad387dec5b2fd0c
Alin Florin <alin@qoffice.ro>
2024-01-21 11:29:37 +0200

    v2

--- src/device/cameramanager.h
+++ src/device/cameramanager.h
@@ -1,10 +1,18 @@
 #ifndef CameraManager_h
 #define CameraManager_h
+
+#include "../lib/OV7670-ESP32/OV7670.h"
+
 class CameraManager {
 public:
     CameraManager();
     ~CameraManager();
     void init();
     void takePhoto();
+private:
+    OV7670* cam;
+    uint16_t *buf;
+    uint16_t w;
+    uint16_t h;
 };
 #endif
